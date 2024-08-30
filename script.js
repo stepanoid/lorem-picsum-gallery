08ddfd70d551c51d0b09028f948eeff73dea047d
stepanoid <123142019+stepanoid@users.noreply.github.com>
2024-08-30 08:43:07 +0300

    add template literals logic

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ if(!items) {
 function initialList() {
   if(container.innerHTML === '') {
     addCard(50);
-    console.log('List was Empty!');
+    // console.log('List was Empty!');
   }
 }
 
@@ -86,16 +86,14 @@ function sortItems() {
 function createCard(i) {
   // create card
   let card = document.createElement('div');
-  card.setAttribute('class', 'card');
+  card.classList.add('card');
 
-  let newImg = document.createElement('img');
-  newImg.setAttribute('src', items[i].image);
-  card.append(newImg);
-
-  let header = document.createElement('div');
-  header.textContent = `${items[i].name} ${items[i].code}`;
-  header.setAttribute('class', 'title');
-  card.append(header);
+  card.innerHTML = `
+    <div class="card">
+      <img src="${items[i].image}" alt="">
+      <div class="title">${items[i].name} ${items[i].code}</div>
+    </div>
+  `;
 
   // card upload menu:
   createUploadMenu(card);
@@ -105,38 +103,24 @@ function createCard(i) {
 
 function createUploadMenu(parentElement) {
   tooltip = document.createElement('div');
-  tooltip.setAttribute('class', 'tooltip');
-  // --resolution group
-  let inputsGroup = document.createElement('div');
-  inputsGroup.setAttribute('class', 'inputsGroup');
-  // --W
-  let labelW = document.createElement('label');
-  let inputW = document.createElement('input');
-  inputW.setAttribute('type', 'number');
-  inputW.setAttribute('class', 'inputW');
-  labelW.textContent = 'width:';
-  inputW.value = 400;
-  labelW.append(inputW);
-  inputsGroup.append(labelW);
-  // --H
-  let labelH = document.createElement('label');
-  let inputH = document.createElement('input');
-  inputH.setAttribute('type', 'number');
-  inputH.setAttribute('class', 'inputH');
-  labelH.textContent = 'height:';
-  inputH.value = 400;
-  labelH.append(inputH);
-  inputsGroup.append(labelH);
-  //
-  tooltip.append(inputsGroup);
-  // --button
-  uploadBtn = document.createElement('button');
-  uploadBtn.textContent = 'copy image'
-  tooltip.append(uploadBtn);
+  tooltip.classList.add('tooltip');
 
+  tooltip.innerHTML = `
+    <div class="inputsGroup">
+      <label>
+        width:
+        <input type="number" class="inputW" value="400">
+      </label>
+      <label>
+        height:
+        <input type="number" class="inputH" value="400">
+      </label>
+    </div>
+    <button>copy image</button>
+  `;
+  
   parentElement.append(tooltip);
-  // setup
-  uploadBtn.addEventListener('click', (e)=>copyUrl(e.currentTarget));
+  parentElement.querySelector('button').addEventListener('click', (e)=>copyUrl(e.currentTarget));
 }
 
 //////////////////////////////
@@ -162,7 +146,7 @@ function copyUrl(element) {
   let imgSrcUpload = `${imgSrc.slice(0, -3)}${currentCard.querySelector('.inputW').value}/${currentCard.querySelector('.inputH').value}`;
   navigator.clipboard.writeText(imgSrcUpload).then(function() {
     currentCard.querySelector('.tooltip').textContent = 'URL copied!';
-    console.log('copied!');
+    // console.log('copied!');
   }).catch(function(err) {
     console.error('Could not copy text: ', err);
   });
